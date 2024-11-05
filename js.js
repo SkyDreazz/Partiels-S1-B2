2ca8b3be39ba14d90e52086b20f22fae93a8643a
Cassoulaid <129860197+Cassoulaid@users.noreply.github.com>
2024-11-05 17:14:00 +0100

    page d'acceuil Axel

--- js.js
+++ js.js
@@ -46,4 +46,62 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const carousels = document.querySelectorAll('.carousel');
     carousels.forEach(initializeCarousel);
+    
+    
+    function Footer(){
+        return `
+        <footer> 
+            <div class='head'>
+                <div>
+                    <h2>Assistance</h2>
+                    <a href='#'>Centre d'aide</a>
+                    <a href='#'>AirCover</a>
+                    <a href='#'>Assistance Handicap</a>
+                    <a href='#'>Options d'annulation</a>
+                    <a href='#'>Soucis de voisinage</a>
+                </div>
+                <div>
+                    <h2>Accueil des voyageurs</h2>
+                    <a href='#'>Mettre votre logement sur Airbnb</a>
+                    <a href='#'>AirCover pour les Hôtes</a>
+                    <a href='#'>Ressource pour les Hôtes</a>
+                    <a href='#'>Forum de la communauté</a>
+                    <a href='#'>Hébergement responsable</a>
+                    <a href='#'>Participez à un cours gratuit sur l'accueil de voyageurs</a>
+                    <a href='#'>Trouver un co-hôte</a>
+                </div>
+                <div>
+                    <h2>Airbnb</h2>
+                    <a href='#'>Newsroom</a>
+                    <a href='#'>Nouvelles fonctionnalités</a>
+                    <a href='#'>Carrières</a>
+                    <a href='#'>Investisseurs</a>
+                    <a href='#'>Cartes Cadeaux</a>
+                    <a href='#'>Séjours d'urgence</a>
+                    <a href='#'>Airbnb.org</a>
+                </div>
+            </div>
+            <div class='foot'>
+                <div>
+                    <p>© 2024 Airbnb, Inc.</p>
+                    <a href='#'>Conditions générales</a>
+                    <a href='#'>Informations sur l'entreprise</a>
+                </div>
+                <div>
+                    <a href='#'>Confidentialité </a>
+                    <a href='#'>Plan du site</a>
+                    <a href='#'>Fonctionnement du site</a>
+                </div>
+                <div>
+                <a href='#'> <img src='images/icons/Facebook.png'> </a>
+                    <a href='#'> <img src='images/icons/TwitterX.png'> </a>
+                    <a href='#'> <img src='images/icons/Instagram.png'> </a>
+                </div>
+            </div>
+        </footer>
+        `;
+    }
+    
+    document.getElementById("footer").innerHTML = Footer();
 });
+
